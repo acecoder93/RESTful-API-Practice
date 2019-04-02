c4a942d8e44d2999abfa37d34e2e04adf301e28e
Anuj <anujsaheba@gmail.com>
2019-04-02 10:59:26 -0500

    updated student controller

--- node-seq/routes/index.js
+++ node-seq/routes/index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 
-const classroomController = require('../controllers').classroom();
+const classroomController = require('../controllers').classroom;
+const studentController = require('../controllers').student;
 
+// Classroom Controller Route
 router.get('/api/classroom', classroomController.list);
 router.get('/api/classroom/:id', classroomController.getById);
 router.post('/api/classroom', classroomController.add);
 router.put('/api/classroom/:id', classroomController.update);
 router.delete('/api/classroom/:id', classroomController.delete);
+
+
+// Student Controller Route
+router.get('/api/student', studentController.list);
+router.get('/api/student/:id', studentController.getById);
+router.post('/api/student', studentController.add);
+router.put('/api/student/:id', studentController.update);
+router.delete('/api/student/:id', studentController.delete);
